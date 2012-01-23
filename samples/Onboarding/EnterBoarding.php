ac65b6ab5cae4f9f4a9366e234ff3bd7675ec77c
ganeshx <chipgeri@gmail.com>
2012-01-23 18:37:58 +0530

    added onboarding samples

---
+++ samples/Onboarding/EnterBoarding.php
@@ -0,0 +1,57 @@
+<?php
+$path = '../../lib';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+require_once('services/PayPalApi/PayPalAPIInterfaceServiceService.php');
+require_once('PPLoggingManager.php');
+
+$logger = new PPLoggingManager('EnterBoarding');
+
+$bankAccount = new BankAccountDetailsType();
+$bankAccount->AccountNumber = $_REQUEST['accNum'];
+$bankAccount->Name = $_REQUEST['accName'];
+$bankAccount->Type = $_REQUEST['accType'];
+
+$bizAddress = new AddressType();
+$bizAddress->Name = $_REQUEST['name'];
+$bizAddress->Street1 = $_REQUEST['street'];
+$bizAddress->CityName = $_REQUEST['city'];
+$bizAddress->StateOrProvince = $_REQUEST['state'];
+$bizAddress->PostalCode = $_REQUEST['postalCode'];
+$bizAddress->Country = $_REQUEST['countryCode'];
+
+$businessInfo = new BusinessInfoType();
+$businessInfo->Name = $_REQUEST['businessName'];
+$businessInfo->Type = $_REQUEST['businessType'];
+$businessInfo->Category = $_REQUEST['businessCategory'];
+$businessInfo->AverageMonthlyVolume = $_REQUEST['averageMonthlyVolume'];
+$businessInfo->AveragePrice = $_REQUEST['averageTransPrice'];
+$businessInfo->RevenueFromOnlineSales = $_REQUEST['revenuePercentage'];
+$businessInfo->Address  = $bizAddress;
+
+$ownerInfo = new BusinessOwnerInfoType();
+$ownerInfo->SSN = $_REQUEST['SSN'];
+$ownerInfo->MobilePhone = $_REQUEST['ownerPhone'];
+
+
+$enterBoardingRequestDetails = new EnterBoardingRequestDetailsType();
+$enterBoardingRequestDetails->ProductList  = $_REQUEST['prodList'];
+$enterBoardingRequestDetails->BankAccount = $bankAccount;
+$enterBoardingRequestDetails->BusinessInfo = $businessInfo;
+$enterBoardingRequestDetails->MarketingCategory = $_REQUEST['marketingCategory'];
+$enterBoardingRequestDetails->OwnerInfo = $ownerInfo;
+$enterBoardingRequestDetails->ProgramCode = $_REQUEST['programCode'];
+
+$enterBoardingRequest = new EnterBoardingRequestType();
+$enterBoardingRequest->EnterBoardingRequestDetails = $enterBoardingRequestDetails;
+$enterBoardingRequest->Version = 84;
+
+$enterBoardingReq = new EnterBoardingReq();
+$enterBoardingReq->EnterBoardingRequest = $enterBoardingRequest;
+
+$paypalService = new PayPalAPIInterfaceServiceService();
+$enterBoardingResponse = $paypalService->EnterBoarding($enterBoardingReq);
+
+echo "<pre>";
+print_r($enterBoardingResponse);
+echo "</pre>";
+require_once '../Response.php';
---
+++ samples/Onboarding/GetBoardingDetails.php
@@ -0,0 +1,22 @@
+<?php
+$path = '../../lib';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+require_once('services/PayPalApi/PayPalAPIInterfaceServiceService.php');
+require_once('PPLoggingManager.php');
+
+$logger = new PPLoggingManager('GetBoardingDetails');
+
+$getBoardingDetailsReqest = new GetBoardingDetailsRequestType();
+$getBoardingDetailsReqest->Token = $_REQUEST['onboardingToken'];
+$getBoardingDetailsReqest->Version = 84;
+
+$getBoardingDetailsReq =  new GetBoardingDetailsReq();
+$getBoardingDetailsReq->GetBoardingDetailsRequest = $getBoardingDetailsReqest;
+
+$paypalService = new PayPalAPIInterfaceServiceService();
+$getBoardingDetailsResponse = $paypalService->GetBoardingDetails($getBoardingDetailsReq);
+
+echo "<pre>";
+print_r($getBoardingDetailsResponse);
+echo "</pre>";
+require_once '../Response.php';
