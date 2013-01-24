e12b238d2eb068393258daa065aa90cabbf8772f
Ganesh Hegde <1ganesh1@facebook.com>
2013-01-24 23:35:45 +0530

    uses autoloader

--- tests/PPAPIServiceTest.php
+++ tests/PPAPIServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once 'PPAPIService.php';
+
 
 /**
  * Test class for PPAPIService.
--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Register autoloader
- */
-require("lib/PayPal_Merchant_SDK_Autoloader.php");
-
-PayPal_Merchant_SDK_Autoloader::register();
