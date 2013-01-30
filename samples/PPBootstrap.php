d5bac513aa9a10f02069494905d09298cde3c9df
lathavairamani <lvairamani@paypal.com>
2013-01-30 15:45:25 +0530

    removed permissions stub files from samples, renamed PPBootstrap.php

--- samples/PPBootstrap.php
+++ samples/PPBootstrap.php
@@ -3,8 +3,8 @@
  *  Include this file in your application 
  *  this file includes autoloader.php if using composer. includes custom actoloader if it is a custom installation of SDK
  */
-define('PP_CONFIG_PATH',dirname(__FILE__).'/config/');
-if(file_exists('vendor/autoload.php'))
+define('PP_CONFIG_PATH',dirname(__FILE__).'/../config/');
+if(file_exists(dirname(__FILE__).'/vendor/autoload.php'))
     require 'vendor/autoload.php';
 
 else
