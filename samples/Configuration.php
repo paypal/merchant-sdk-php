c85604a22102a4750589f4c9f81fbfcc684526f4
Ganesh Hegde <1ganesh1@facebook.com>
2013-07-09 10:02:07 +0530

    rearranging config map

--- samples/Configuration.php
+++ samples/Configuration.php
@@ -3,6 +3,9 @@
 	public static function getSignatureConfig()
 	{
 		$config = array(
+				// values: 'sandbox' for testing
+				//		   'live' for production
+				"mode" => "sandbox",
 				"acct1.UserName" => "jb-us-seller_api1.paypal.com",
 				"acct1.Password" => "WX4WTU3S8MY44S7F",
 				"acct1.Signature" => "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy",
@@ -10,35 +13,21 @@
 				"acct1.AppId" => "APP-80W284485P519543T",
 				// Subject is optional and is required only in case of third party authorization
 				//"acct1.Subject" => "",
-				// values: 'sandbox' for testing
-				//		   'live' for production
-				"mode" => "sandbox",
-			
-				//Connection Information
-				"http.ConnectionTimeOut" => "30",
-				"http.Retry" => "2",
-				//http.Proxy=http://[username:password]@hostname[:port][/path]
 				);
 		return $config;
 	}
 	public static function getCertificateConfig()
 	{
 		$config = array(
+				// values: 'sandbox' for testing
+				//		   'live' for production
+				"mode" => "sandbox",
 				"acct1.UserName" => "certuser_biz_api1.paypal.com",
 				"acct1.Password" => "D6JNKKULHN3G5B8A",
 				// Certificate path relative to config folder or absolute path in file system
 				"acct1.CertPath" => "cert_key.pem",
 				// AppId required if permission APIs are used
 				"acct1.AppId" => "APP-80W284485P519543T",
-		
-				// values: 'sandbox' for testing
-				//		   'live' for production
-				"mode" => "sandbox",
-		
-				//Connection Information
-				"http.ConnectionTimeOut" => "30",
-				"http.Retry" => "2",
-				//http.Proxy=http://[username:password]@hostname[:port][/path]
 		);	
 		return $config;
 	}
