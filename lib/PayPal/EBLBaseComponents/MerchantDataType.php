43e53861883f333efb529918a5c3e4d151f06a56
lathavairamani <lvairamani@paypal.com>
2013-08-22 23:39:21 +0530

    updated stubs

---
+++ lib/PayPal/EBLBaseComponents/MerchantDataType.php
@@ -0,0 +1,25 @@
+<?php 
+namespace PayPal\EBLBaseComponents;
+use PayPal\Core\PPXmlMessage;
+/**
+ * This holds all key-value pairs which merchants wants to pass
+ * it to the open wallet(PLCC) processor. 
+ */
+class MerchantDataType  
+   extends PPXmlMessage{
+
+	/**
+	 * 
+     * @array
+	 * @access public
+	 
+	 * @namespace ebl
+	 
+	 	 	 	 
+	 * @var PayPal\EBLBaseComponents\TupleType	 
+	 */ 
+	public $MerchantDataTuple;
+
+
+    
+}
---
+++ lib/PayPal/EBLBaseComponents/TupleType.php
@@ -0,0 +1,42 @@
+<?php 
+namespace PayPal\EBLBaseComponents;
+use PayPal\Core\PPXmlMessage;
+/**
+ * This holds single key-value pair. 
+ */
+class TupleType  
+   extends PPXmlMessage{
+
+	/**
+	 * Key Name.Optional
+	 * @access public
+	 
+	 * @namespace ebl
+	 
+	 	 	 	 
+	 * @var string	 
+	 */ 
+	public $Key;
+
+	/**
+	 * Value for the above key.Optional
+	 * @access public
+	 
+	 * @namespace ebl
+	 
+	 	 	 	 
+	 * @var string	 
+	 */ 
+	public $Value;
+
+	/**
+	 * Constructor with arguments
+	 */
+	public function __construct($Key = NULL, $Value = NULL) {
+		$this->Key = $Key;
+		$this->Value = $Value;
+	}
+
+
+    
+}
