430d10e5be686a6da006700d50c4c766b307bfc1
Gerren Scoon <gscoon@gmail.com>
2019-10-11 00:07:30 -0400

    Update AbstractResponseType.php

--- lib/PayPal/EBLBaseComponents/AbstractResponseType.php
+++ lib/PayPal/EBLBaseComponents/AbstractResponseType.php
@@ -2,6 +2,7 @@
 namespace PayPal\EBLBaseComponents;
 
 use PayPal\Core\PPXmlMessage;
+Use PayPal\EBLBaseComponents\ErrorType;
 
 /**
  * Base type definition of a response payload that can carry
@@ -68,8 +69,93 @@ class AbstractResponseType
      */
     public $Build;
 
-    public $LongMessage;
-    
-    public $ShortMessage;
+    /**
+     * @param array  $map    intermediate array representation of XML message to deserialize
+     * @param string $isRoot true if this is a root class for SOAP deserialization
+     */
+    public function init(array $map = array(), $isRoot = true)
+    {
+        if ($isRoot) {
+            if (stristr($map[0]['name'], ":fault")) {
+                throw new PPTransformerException("soapfault");
+            } else {
+                $map = $map[0]['children'];
+            }
+        }
 
+        if (empty($map)) {
+            return;
+        }
+
+        if (($first = reset($map)) && !is_array($first) && !is_numeric(key($map))) {
+            parent::init($map, false);
+            return;
+        }
+
+        $propertiesMap = PPUtils::objectProperties($this);
+        $arrayCtr      = array();
+        foreach ($map as $element) {
+
+            if (empty($element) || empty($element['name'])) {
+                continue;
+            } elseif (!array_key_exists($property = strtolower($element['name']), $propertiesMap)) {
+                if (!preg_match('~^(.+)[\[\(](\d+)[\]\)]$~', $property, $m)) {
+                    continue;
+                }
+
+                $element['name'] = $m[1];
+                $element['num']  = $m[2];
+            }
+            $element['name'] = $propertiesMap[strtolower($element['name'])];
+            if (PPUtils::isPropertyArray($this, $element['name'])) {
+                $arrayCtr[$element['name']] = isset($arrayCtr[$element['name']]) ? ($arrayCtr[$element['name']] + 1) : 0;
+                $element['num']             = $arrayCtr[$element['name']];
+            }
+            if (!empty($element["attributes"]) && is_array($element["attributes"])) {
+                foreach ($element["attributes"] as $key => $val) {
+                    $element["children"][] = array(
+                      'name' => $key,
+                      'text' => $val,
+                    );
+                }
+
+                if (isset($element['text'])) {
+                    $element["children"][] = array(
+                      'name' => 'value',
+                      'text' => $element['text'],
+                    );
+                }
+
+                $this->fillRelation($element['name'], $element);
+            } elseif (isset($element['text']) && !is_null($element['text'])) {
+                if (isset($element['num'])) {
+                    $this->{$element['name']}[$element['num']] = $element['text'];
+                } else {
+                    $this->{$element['name']} = $element['text'];
+                }
+            } elseif (!empty($element["children"]) && is_array($element["children"])) {
+                $this->fillRelation($element['name'], $element);
+            }
+        }
+    }
+
+    public function fillRelation($property, array $element)
+    {
+        $type = PPUtils::propertyType($this, $property);
+
+        if ($type === 'string' && $property === 'Errors') {
+            $type = ErrorType::class;
+        } elseif (!class_exists($type)) {
+            trigger_error("Class $type not found.", E_USER_NOTICE);
+            return; // just ignore
+        }
+
+        if (isset($element['num'])) { // array of objects
+            $this->{$property}[$element['num']] = $item = new $type();
+            $item->init($element['children'], false);
+        } else {
+            $this->{$property} = new $type();
+            $this->{$property}->init($element["children"], false);
+        }
+    }
 }
